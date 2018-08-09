85cc13dea240af639f81deb9ce18bb1752d03d84
mhmdAlnajjar <42078502+mhmdAlnajjar@users.noreply.github.com>
2018-08-09 23:42:03 +0300

    add  Delete function

--- app/src/main/java/com/example/mis/carmaintenance/MainActivity.kt
+++ app/src/main/java/com/example/mis/carmaintenance/MainActivity.kt
@@ -7,10 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
     fun Add()
     {
-        
+
+    }
+    fun Delete()
+    {
+
     }
 }
